7a0970c16c5b16734614d985522fce51b2c6b482
mzgcz <j.wenjiao@gmail.com>
2014-03-25 17:37:33 +0800

    完成移动版播放页面

--- lib/SelfMusic/Mobile.pm
+++ lib/SelfMusic/Mobile.pm
@@ -1,5 +1,9 @@
 package SelfMusic::Mobile;
+use utf8;
+use SelfConf;
 use Mojo::Base 'Mojolicious::Controller';
+
+my $self_net = SelfConf::NET_ADDR;
 
 sub index {
   my $self = shift;
@@ -19,18 +23,30 @@ sub index {
 
     my $self_plist = $self->self_db->get_user_plist($user);
     @play_list = SelfCommon::sort_by_pinyin(keys %{$self_plist->{"music"}});
-    $self->stash(puser => $self->self_register->encode_user($user));
-    $self->stash(pid => $id);
   }
 
   $self->stash(response => $response);
   $self->stash(rspn_type => $rspn_type);
   $self->stash(plist => \@play_list);
+  $self->stash(puser => $self->self_register->encode_user($user));
+  $self->stash(pid => $id);
+  $self->stash(pnet => $self_net);
   $self->render();
 }
 
 sub play {
   my $self = shift;
+  my $user = $self->self_register->decode_user($self->param('user'));
+  my $id = $self->param('id');
+  my $item = $self->param('item');
+  my $self_plist = $self->self_db->get_user_plist($user);
+
+  $self->stash(pitem => $item);
+  $self->stash(list => $self_plist->{"music"}->{$item});
+  $self->stash(path => $self_plist->{"path"}->{$item});
+  $self->stash(puser => $self->self_register->encode_user($user));
+  $self->stash(pid => $id);
+  $self->stash(pnet => $self_net);
 
   $self->render();
 }
